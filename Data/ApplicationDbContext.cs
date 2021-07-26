37f848622f7c11bd075469fd0ff00bf451198d43
Rishi Raj <rishikpr007@gmail.com>
2021-07-26 11:42:48 +0530

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CodeFirstEF.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirstEF.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Company> Companies { get; set; }
+
+
+    }
+}
---
+++ Models/Company.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirstEF.Models
+{
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Company name is required.")]
+        [Display(Name = "Company Name")]
+        public String CompanyName { get; set; }
+
+    }
+}
